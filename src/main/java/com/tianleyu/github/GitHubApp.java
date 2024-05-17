a84fecf14fea57021c5bdd0f0b10f795f2523ff5
Tianle Yu <yulele28@126.com>
2024-05-17 05:27:33 -0700

    [feat] init of GitAppOrg

--- src/main/java/com/tianleyu/github/GitHubApp.java
+++ src/main/java/com/tianleyu/github/GitHubApp.java
@@ -30,7 +30,8 @@ public class GitHubApp {
         if (resp.statusCode() != 200) {
             throw new GitHubAppException("Organization not found");
         }
-        return new GitHubAppOrg(this, org);
+        JSONObject json = new JSONObject(resp.body());
+        return new GitHubAppOrg(this, org, json.getString("id"));
     }
 
     public HttpResponse<String> get(String url)
@@ -61,6 +62,24 @@ public class GitHubApp {
                     .header("X-GitHub-Api-Version", "2022-11-28")
                     .header("Content-Type", "application/json")
                     .POST(HttpRequest.BodyPublishers.ofString(body.toString()))
+                    .build();
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            throw new GitHubAppException(e.getMessage());
+        }
+    }
+
+    public HttpResponse<String> post(String url, String body)
+            throws GitHubAppException {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://api.github.com" + url))
+                    .timeout(Duration.of(10L, ChronoUnit.SECONDS)) // Change 10 to 10L
+                    .header("Authorization", "Bearer " + jwtProvider.getJwt())
+                    .header("Accept", "application/vnd.github.v3+json")
+                    .header("X-GitHub-Api-Version", "2022-11-28")
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(body))
                     .build();
             return client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (Exception e) {
--- src/main/java/com/tianleyu/github/GitHubAppOrg.java
+++ src/main/java/com/tianleyu/github/GitHubAppOrg.java
@@ -1,11 +1,23 @@
 package com.tianleyu.github;
 
+import org.json.*;
+
 public class GitHubAppOrg {
     public String orgId;
     private GitHubApp app;
+    private String instId;
+    private String accessToken;
 
-    public GitHubAppOrg(GitHubApp app, String org) {
+    public GitHubAppOrg(GitHubApp app, String org, String instId) {
         this.orgId = org;
         this.app = app;
+        this.instId = instId;
+        getInstallationAccessToken();
+    }
+
+    private void getInstallationAccessToken() {
+        String resp = app.post("/app/installations/" + instId + "/access_tokens", "{}").body();
+        JSONObject json = new JSONObject(resp);
+        this.accessToken = json.getString("token");
     }
 }
