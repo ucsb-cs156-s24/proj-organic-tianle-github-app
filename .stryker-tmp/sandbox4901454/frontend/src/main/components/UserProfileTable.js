7d161f9f55f3d3e20851685cc85539898ac02ff1
Xalex217 <49774761+Xalex217@users.noreply.github.com>
2024-05-25 20:51:05 -0700

    Delete .stryker-tmp directory

--- .stryker-tmp/sandbox4901454/frontend/src/main/components/UserProfileTable.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactJson from "react-json-view";
-
-export default function UserProfileTable({ user }) {
-    return (
-        <ReactJson src={user} />
-    );
-}
--- .stryker-tmp/sandbox4901454/src/main/java/edu/ucsb/cs156/organic/services/SystemInfoService.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsb.cs156.organic.services;
-
-import edu.ucsb.cs156.organic.models.SystemInfo;
-
-public abstract class SystemInfoService {
-  public abstract SystemInfo getSystemInfo();
-}
