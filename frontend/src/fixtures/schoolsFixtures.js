21fb7cb6d03b377c71a547498b9e749a56d33298
hylee <hylee@ucsb.edu>
2024-05-29 20:33:58 -0700

    readding files from Aaron-UX onto new branch

--- frontend/src/fixtures/schoolsFixtures.js
+++ frontend/src/fixtures/schoolsFixtures.js
@@ -3,30 +3,26 @@ const schoolsFixtures = {
         "abbrev": "ucsb",
         "name": "UC Santa Barbara",
         "termRegex": "[WSMF]\\d\\d",
-        "termDescription": "Enter quarter, e.g. F23, W24, S24, M24",
-        "termError": "Quarter must be entered in the correct format"
+        "termDescription": "Enter quarter, e.g. F23, W24, S24, M24"
     },
     threeSchools: [
         { 
             "abbrev": "ucsb",
             "name": "UC Santa Barbara",
             "termRegex": "[WSMF]\\d\\d",
-            "termDescription": "Enter quarter, e.g. F23, W24, S24, M24",
-            "termError": "Quarter must be entered in the correct format"
+            "termDescription": "Enter quarter, e.g. F23, W24, S24, M24"
         },
         {
             "abbrev": "umn",
             "name": "University of Minnesota",
             "termRegex": "[WSMF]\\d\\d",
-            "termDescription": "Enter quarter, e.g. F23, W24, S24, M24",
-            "termError": "Quarter must be entered in the correct format"
+            "termDescription": "Enter quarter, e.g. F23, W24, S24, M24"
         },
         {
             "abbrev": "ucsd",
             "name": "UC San Diego",
             "termRegex": "[WSMF]\\d\\d",
-            "termDescription": "Enter quarter, e.g. F23, W24, S24, M24",
-            "termError": "Quarter must be entered in the correct format"
+            "termDescription": "Enter quarter, e.g. F23, W24, S24, M24"
         }
     ]
 };
